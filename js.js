158b5a3f45eebc8aad9b79f9a0b15000b4b1bf81
never333 <goon815@gmail.com>
2016-09-29 16:24:45 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,71 @@
+function hienhv(){
+	hien=document.getElementById("hoavip");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function hienhty(){
+	hien=document.getElementById("hoaty");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function hienhc(){
+	hien=document.getElementById("hoac");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function hienhsn(){
+	hien=document.getElementById("hoasn");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function hienhvp(){
+	hien=document.getElementById("hoavp");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function hienhdt(){
+	hien=document.getElementById("hoadt");
+	hien.style.visibility="visible";
+	hien.style.position="inherit";
+}
+
+function anhv(){
+	an=document.getElementById("hoavip");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
+
+function anhty(){
+	an=document.getElementById("hoaty");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
+
+function anhc(){
+	an=document.getElementById("hoac");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
+
+function anhsn(){
+	an=document.getElementById("hoasn");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
+
+function anhvp(){
+	an=document.getElementById("hoavp");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
+
+function anhdt(){
+	an=document.getElementById("hoadt");
+	an.style.visibility="hidden";
+	an.style.position="absolute";
+}
